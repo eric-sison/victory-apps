66538411dbdee86dffee86c91b68fa202fd5b6ee
Eric Sison <ericsison.dev@gmail.com>
2026-05-05 18:03:57 +0800

    feat(web): added gitignore

--- apps/web/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"oul+Xf5AKJcBeojt5SMF31YJIAwY8GygeRTb230Bag8=\"\n}"
--- apps/web/.next/dev/static/chunks/apps_web_app_layout_tsx_f8e2e802._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__2f30db1c._.css",
-  "static/chunks/_b7f74c4c._.js"
-],
-    source: "dynamic"
-});
