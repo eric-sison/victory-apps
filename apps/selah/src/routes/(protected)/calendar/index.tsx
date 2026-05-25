db68629d0abda35c5e89b9fd37f5489d7e58e884
Eric Sison <ericsison.dev@gmail.com>
2026-05-26 03:44:18 +0800

    chore: formatted files

--- apps/selah/src/routes/(protected)/calendar/index.tsx
+++ apps/selah/src/routes/(protected)/calendar/index.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/(protected)/calendar/')({
+export const Route = createFileRoute("/(protected)/calendar/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/(protected)/calendar/"!</div>
+  return <div>Hello "/(protected)/calendar/"!</div>;
 }
--- apps/selah/src/routes/(protected)/services/index.tsx
+++ apps/selah/src/routes/(protected)/services/index.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/(protected)/services/')({
+export const Route = createFileRoute("/(protected)/services/")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/(protected)/services/"!</div>
+  return <div>Hello "/(protected)/services/"!</div>;
 }
